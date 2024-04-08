5e91847a90248bdef903fc072afe91c3d54621a6
theoshka <147602076+theoshka@users.noreply.github.com>
2024-04-08 20:08:22 +0100

    Algorithm for hashing inputs
    Created a class that deals with applying the hashing algorithm to an input.

---
+++ src/java/Hasher.java
@@ -0,0 +1,43 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+
+public class Hasher {
+
+    public String getHashedValue(String input) {
+        
+        try {
+            return toHexString(getSHA(input));
+        } catch (NoSuchAlgorithmException e) {
+            
+            return("exception thrown " + e);
+        }
+
+    }
+
+    private static byte[] getSHA(String input) throws NoSuchAlgorithmException {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static String toHexString(byte[] hash) {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 64) {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
+    }
+}
